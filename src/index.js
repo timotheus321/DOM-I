96d3a95e22347d1f281809632026d26860c2c63a
Timothy White <bigtball321@yahoo.com>
2023-03-02 11:38:15 -0500

    about half way through the assignment

--- src/index.js
+++ src/index.js
@@ -38,5 +38,55 @@ const siteContent = { // DO NOT CHANGE THIS OBJECT
     "accent-img": "http://localhost:9000/img/accent.png",
   },
 };
+// Images
+const logoImg = document.querySelector('#logo-img')
+logoImg.src = siteContent.images['logo-img']
 
-console.log('project wired!')
+const ctaImg = document.querySelector('#cta-img')
+ctaImg.src = siteContent.images['cta-img']
+
+const midImg = document.querySelector('#middle-img')
+midImg.src = siteContent.images['accent-img']
+
+// Footer Link
+const footerLink = document.querySelector('footer a')
+footerLink.textContent = siteContent.footer.copyright
+footerLink.classList.add('bold')
+
+
+//contact
+const contact = document.querySelector('section.contact')
+//contact.children[0].textContent = siteContent.contact['contact-h4']
+contact.querySelector('h4').textContent = siteContent.contact['contact-h4']
+contact.children[1].textContent = siteContent.contact['address']
+contact.querySelector('p:nth-of-type(2)').textContent = siteContent.contact['phone']
+contact.children[3].textContent = siteContent.contact['email']
+// Top content
+const topContent = document.querySelector('.top-content')
+topContent.children[0].children[0].textContent = siteContent['main-content']['features-h4']
+topContent.children[0].children[1].textContent = siteContent['main-content']['features-content']
+topContent.children[1].children[0].textContent = siteContent['main-content']['about-h4']
+topContent.children[1].children[1].textContent = siteContent['main-content']['about-content']
+
+// Bottom Content
+const bottomContent = document.querySelector('.bottom-content')
+const h4sBottom = bottomContent.querySelectorAll('h4')
+h4sBottom[0].textContent = siteContent['main-content']['services-h4']
+h4sBottom[1].textContent = siteContent['main-content']['property-h4']
+h4sBottom[2].textContent = siteContent['main-content']['vision-h4']
+const psBottom = bottomContent.querySelectorAll('p')
+psBottom[0].textContent = siteContent['main-content']['services-content']
+psBottom[1].textContent = siteContent['main-content']['property-content']
+psBottom[2].textContent = siteContent['main-content']['vision-content']
+
+// cta
+document.querySelector('.cta .cta-text h1').textContent = siteContent.h1
+document.querySelector('.cta .cta-text button').textContent = siteContent.cta.button
+
+// nav links
+const navLinks = document.querySelectorAll('header nav a')
+const navLinksTexts = Object.values(siteContent.nav)
+navLinks.forEach((link,idx) =>{
+    link.textContent = navLinksTexts[idx]
+    link.classList.add('italic')
+})
